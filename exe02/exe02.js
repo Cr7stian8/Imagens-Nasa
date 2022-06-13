39ceed558872b2c1bef9692cd6a762d7ee58f886
Cr7stian8 <cristianotropeiro@email.com>
2022-06-13 15:37:20 -0300

    Adição exercício 02

---
+++ exe02/exe02.js
@@ -0,0 +1,17 @@
+//2 – Faça um programa que entre com cinco números e imprima o quadrado de cada número.
+
+//Declarando a variável num1 = número 1 ou o primeiro número
+num1 = prompt("Digite um número")
+//Alertando o resultado do número ao quadrado
+alert("número "+num1+" ao quadrado: "+num1*num1)
+//Foi utilizado num 1 * num 1 já que um número ao quadrado nada mais é do que ele vezes ele mesmo
+
+//A mesma lógica foi aplicada aos outros números 
+num2 = prompt("Digite um número")
+alert("número "+num2+" ao quadrado: "+num2*num2)
+num3 = prompt("Digite um número")
+alert("número "+num3+" ao quadrado: "+num3*num3)
+num4 = prompt("Digite um número")
+alert("número "+num4+" ao quadrado: "+ num4*num4)
+num5 = prompt("Digite um número")
+alert("número "+num5+ " ao quadrado: "+ num5*num5)
