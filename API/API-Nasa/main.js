1e1010697890bc08bd28fd55ca881acc841ff9c8
Cr7stian8 <cristianotropeiro@gmail.com>
2022-09-03 18:18:41 -0300

    Metodo GET com base na data

--- API/API-Nasa/main.js
+++ API/API-Nasa/main.js
@@ -1,18 +1,28 @@
 function principal() {
-    let urlAPI = "https://api.nasa.gov/planetary/apod?api_key=XNDfJe9BMgcT1s0MXNp3RC4kworzZekn9ORy9hYu";
+    let urlAPI = "https://api.nasa.gov/planetary/apod?date=2000-08-13&api_key=XNDfJe9BMgcT1s0MXNp3RC4kworzZekn9ORy9hYu";
 
     dados = get(urlAPI);
     dadosJson = JSON.parse(dados);
 
     console.log(dadosJson.title);
-    console.log(dadosJson.hdurl);
 
+    // Adicionando Titulo
+    var titulo = document.createElement("h2");
+    titulo.innerHTML = dadosJson.title;
+    document.getElementById("div").appendChild(titulo);
+
+    // Adicionando Imagem
     var imagem = document.createElement("img");
     imagem.src = dadosJson.hdurl;
-    
-    document.getElementById("IdImagem").appendChild(imagem);
+    document.getElementById("div").appendChild(imagem);
+
+    //Adicionando data
+    var data = document.createElement("p")
+    data.innerHTML = dadosJson.date;
+    document.getElementById("div").appendChild(data)
 }
 
+// MÉTODOS HTTP
 function get(url) {
     let request = new XMLHttpRequest();
     request.open("GET", url, false);
