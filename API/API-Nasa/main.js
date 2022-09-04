6b9175c542cc65e037300d15d9729a4f39dfbf39
Cr7stian8 <cristianotropeiro@gmail.com>
2022-09-04 17:53:15 -0300

    implementando busca pelo site

--- API/API-Nasa/main.js
+++ API/API-Nasa/main.js
@@ -1,25 +1,28 @@
 function principal() {
-    let urlAPI = "https://api.nasa.gov/planetary/apod?date=2000-08-13&api_key=XNDfJe9BMgcT1s0MXNp3RC4kworzZekn9ORy9hYu";
+    //Api que está sendo consumida
+    var url = "https://api.nasa.gov/planetary/apod?api_key=XNDfJe9BMgcT1s0MXNp3RC4kworzZekn9ORy9hYu";
 
+    //Recebendo valor dos input
+    var dia = document.getElementById("dia").value;
+    var mes = document.getElementById("mes").value;
+    var ano = document.getElementById("ano").value;
+
+    var data ="&date="+ano+"-"+mes+"-"+dia;
+    let urlAPI = url + data;
+
+    //Passando dados para JSON para facilitar a manipulação
     dados = get(urlAPI);
     dadosJson = JSON.parse(dados);
 
-    console.log(dadosJson.title);
-
-    // Adicionando Titulo
-    var titulo = document.createElement("h2");
-    titulo.innerHTML = dadosJson.title;
-    document.getElementById("div").appendChild(titulo);
-
-    // Adicionando Imagem
-    var imagem = document.createElement("img");
-    imagem.src = dadosJson.hdurl;
-    document.getElementById("div").appendChild(imagem);
-
-    //Adicionando data
-    var data = document.createElement("p")
-    data.innerHTML = dadosJson.date;
-    document.getElementById("div").appendChild(data)
+    AdicionaTitulo()
+    AdicionaImagem()
+    AdicionaData()
+}
+function teste(){
+    // ESSE TESTE CONSISTE EM TENTAR DEIXAR SÓ O ULTIMO GET NA TELA
+    document.getElementById("div").remove();
+    let novaDiv = document.createElement("div");
+    novaDiv.id("div");
 }
 
 // MÉTODOS HTTP
@@ -29,5 +32,19 @@ function get(url) {
     request.send();
     return request.responseText;
 }
-
-principal();
+// FUNÇÕES FATORADAS
+function AdicionaData(){
+    var data = document.createElement("p")
+    data.innerHTML = dadosJson.date;
+    document.getElementById("div").appendChild(data)
+}
+function AdicionaImagem(){
+    var imagem = document.createElement("img");
+    imagem.src = dadosJson.hdurl;
+    document.getElementById("div").appendChild(imagem);
+}
+function AdicionaTitulo(){
+    var titulo = document.createElement("h2");
+    titulo.innerHTML = dadosJson.title;
+    document.getElementById("div").appendChild(titulo);
+}
