961111abddb6377d87944b677f8602329a8d723c
Cr7stian8 <cristianotropeiro@gmail.com>
2022-10-04 11:22:20 -0300

    segundos ok

--- Cronômetro/index.js
+++ Cronômetro/index.js
@@ -1,6 +1,19 @@
+//Declaração de variáveis
 var segundosRodando = 0;
-function main() {
-    segundosRodando += 1;
+
+//Função assíncrona dos segundos
+async function seconds(){
     let segundos = document.getElementById("segundos");
-    segundos.innerHTML = segundosRodando;
+    for (let i = 0; i < 60; i++) {
+        segundosRodando += 1;
+        segundos.innerHTML = segundosRodando;
+        await delay(1);
+    }
+}
+
+// Função responsável por fazer esperar
+function delay(n){
+    return new Promise(function(resolve){
+        setTimeout(resolve,n*1000);
+    });
 }
