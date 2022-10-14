d6fedc1cfcd169c3d530be808501b435e1d4e342
Cr7stian8 <cristianotropeiro@gmail.com>
2022-10-14 16:55:32 -0300

    Alterações no cronômetro

--- Cronômetro/index.js
+++ Cronômetro/index.js
@@ -1,14 +1,26 @@
 //Declaração de variáveis
 var segundosRodando = 0;
+var minutosRodando = 0;
+
+function iniciar(){
+    seconds();
+}
 
 //Função assíncrona dos segundos
 async function seconds(){
+    let x = 0;
+    while(x == 0){
     let segundos = document.getElementById("segundos");
     for (let i = 0; i < 60; i++) {
-        segundosRodando += 1;
         segundos.innerHTML = segundosRodando;
         await delay(1);
+        segundosRodando += 1;
     }
+    if (segundosRodando == 60){
+        let minutos = document.getElementById("minutos")
+        minutosRodando += 1;
+        minutos.innerHTML = minutosRodando;
+    }}
 }
 
 // Função responsável por fazer esperar
