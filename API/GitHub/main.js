7704c345fa8853e107b945c24be08857d2d0878c
Cr7stian8 <cristianotropeiro@gmail.com>
2022-10-17 10:22:33 -0300

    commit inicial api github

---
+++ API/GitHub/main.js
@@ -0,0 +1,5 @@
+function main(){
+    var url = "https://api.github.com/search/repositories?q=";
+    var nomeBuscado = document.getElementById("input").value;
+    let urlApi = url + nomeBuscado;
+}
