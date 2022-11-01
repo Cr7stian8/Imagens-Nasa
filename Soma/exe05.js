493c1422a0fda0d19b6ef36c83f7fc3a50622489
Cr7stian8 <cristianotropeiro@gmail.com>
2022-11-01 13:06:59 -0300

    Realizando soma só com um botão

--- Soma/exe05.js
+++ Soma/exe05.js
@@ -1,41 +1,12 @@
-
-//Recebendo o valor do primeiro botão
-function recebe(){
-    //Recebendo valor 
-    numero1 = document.getElementById("textoDigitado").value;
-
-    //Verificando se o valor digitado é um número
-    if(isNaN(numero1) == false ){
-        alert("Primeiro valor adicionado");
-    }else{
-        alert("O valor digitado não é um número");
-    }
-
-}
-
-function recebe2(){
-
-    //recebendo o valor do segundo botão
-    numero2 = document.getElementById("textoDigitado2").value;
-
-    //Verificando se o valor digitado é um número
-    if(isNaN(numero2) == false ){
-        alert("Segundo valor adicionado");
-    }else{
-        alert("O valor digitado não é um número");
-    }
-}
-
 //Somando os dois valores
 function somar(){
-
+    recebe();
+    recebe2();
     //Passando os valores adicionados para inteiro
     numero1 = parseInt(numero1);
     numero2 = parseInt(numero2);
-    
     //Realizando a soma dos números
     var soma = numero1+numero2;
-
     // Verificando se o valor digitado é um número ou string
     if(isNaN(soma)){
         //O que fazer caso seja uma String
@@ -45,4 +16,25 @@ function somar(){
     }else{
         alert("O valor da soma é: "+soma)
     }
-} 
+}
+//Recebendo o valor do primeiro botão
+function recebe(){
+    //Recebendo valor
+    numero1 = document.getElementById("textoDigitado").value;
+    //Verificando se o valor digitado é um número
+    if(isNaN(numero1) == false ){
+        alert("Primeiro valor adicionado");
+    }else{
+        alert("O valor digitado não é um número");
+    }
+}
+function recebe2(){
+    //recebendo o valor do segundo botão
+    numero2 = document.getElementById("textoDigitado2").value;
+    //Verificando se o valor digitado é um número
+    if(isNaN(numero2) == false ){
+        alert("Segundo valor adicionado");
+    }else{
+        alert("O valor digitado não é um número");
+    }
+}
