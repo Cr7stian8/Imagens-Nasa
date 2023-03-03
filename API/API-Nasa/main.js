125d6bd3537d1ba5428e333568572314c277b194
Cr7stian8 <cristianotropeiro@gmail.com>
2023-03-03 09:52:42 -0300

    melhorias JS

--- API/API-Nasa/main.js
+++ API/API-Nasa/main.js
@@ -1,58 +1,55 @@
+function limpa(){
+  resultado.innerHTML = ""
+}
 function principal() {
   //Api que está sendo consumida
-  var url = "https://api.nasa.gov/planetary/apod?api_key=XNDfJe9BMgcT1s0MXNp3RC4kworzZekn9ORy9hYu";
+  var url = "https://api.nasa.gov/planetary/apod?api_key=XNDfJe9BMgcT1s0MXNp3RC4kworzZekn9ORy9hYu"
 
   //Recebendo valor dos input
-  var dia = document.getElementById("dia").value;
-  var mes = document.getElementById("mes").value;
-  var ano = document.getElementById("ano").value;
+  var dia = document.getElementById("dia").value
+  var mes = document.getElementById("mes").value
+  var ano = document.getElementById("ano").value
 
   //Adicionando data a url para realizar o get na data especificada pelo usuario
-  var data ="&date="+ano+"-"+mes+"-"+dia;
-  let urlAPI = url + data;
+  var data ="&date="+ano+"-"+mes+"-"+dia
+  let urlAPI = url + data
 
   //Passando dados para JSON para facilitar a manipulação
-  dados = get(urlAPI);
-  dadosJson = JSON.parse(dados);
+  dados = get(urlAPI)
+  dadosJson = JSON.parse(dados)
 
-  let element = document.getElementById("imagem");
-  if (element == null){
-      conteudo();
-  }else{
-      LimpaTudo();
+  try {
+    conteudo()
+  } catch (error) {
+    LimpaTudo()
   }
 }
 function conteudo(){
-  cria();
-  AdicionaTitulo();
-  AdicionaConteudo();
-  AdicionaImagem();
-  AdicionaData();
+  cria()
+  AdicionaTitulo()
+  AdicionaConteudo()
+  AdicionaImagem()
+  AdicionaData()
 }
-// ============== FUNÇÕES AUTOEXPLICATIVAS ============= //
-function cria(){
-  var imagem = document.createElement("article");
-  imagem.setAttribute("id","imagem")
-  document.getElementsByClassName("resultado")[0].appendChild(imagem);
 
-  var conteudo = document.createElement("article");
-  conteudo.setAttribute("id","conteudo")
-  document.getElementsByClassName("resultado")[0].appendChild(conteudo);
+const resultado = document.getElementById("resultado")
+
+function cria(){
+  // limpando tela antes de realizar outra pesquisa
+  resultado.innerHTML = ""
+  const imagem = document.createElement("img")
+  imagem.src = dadosJson.hdurl
+  imagem.id = "imagemPesquisa"
+  resultado.appendChild(imagem)
+
+  const conteudo = document.createElement("p")
+  conteudo.id = "conteudo"
+  resultado.appendChild(conteudo)
 }
-// =================== FUNÇÕES FATORADAS ============= //
-function AdicionaImagem(){
-  var imagem = document.createElement("img");
-  imagem.src = dadosJson.hdurl;
-  document.getElementById("imagem").appendChild(imagem);
-}
-function LimpaImagem(){
-  var imagem = document.getElementById("imagem");
-  imagem.parentNode.removeChild(imagem);
-}
-// ======================================= //
+
 function AdicionaConteudo(){
-  var descricao = document.createElement("p");
-  descricao.innerHTML = dadosJson.explanation;
+  const descricao = document.createElement("p");
+  descricao.textContent = dadosJson.explanation;
   document.getElementById("conteudo").appendChild(descricao);
   descricao.classList.add("descricao");
 }
@@ -78,9 +75,3 @@ function get(url) {
   request.send();
   return request.responseText;
 }
-function LimpaTudo(){
-  for (var index = 0; index < 3; index++) {
-      LimpaImagem();
-      LimpaConteudo();
-  }
-};
